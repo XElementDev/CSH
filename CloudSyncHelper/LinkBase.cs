05d3d8ae66b2aeb1faf05edd67917a349023bc6a
CSp <christian.sporer@outlook.com>
2015-08-02 18:47:41 +0200

    * fixed Pfusch from last commit (I forgot to commit some files)

---
+++ CloudSyncHelper/LinkBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace XElement.CloudSyncHelper
+{
+    public abstract class LinkBase : ILink
+    {
+        public void /*ILink.*/Do()
+        {
+            var mkLink = String.Format( "MKLINK /? {0}", this._mkLinkParams );
+            var process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = "/c " + mkLink;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+
+            this.StandardOutput = process.StandardOutput.ReadToEnd();
+        }
+
+        public string /*ILink.*/StandardOutput { get; private set; }
+
+        public void /*ILink.*/Undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract string _mkLinkParams { get; }
+    }
+}
