5680da2ed0a5f7da84089c863df5c48cd452acc9
CSp <christian.sporer@outlook.com>
2015-08-02 18:04:43 +0200

    * whitespace fix

--- Serializiation/DataTypes.cs
+++ Serializiation/DataTypes.cs
@@ -58,7 +58,7 @@ namespace XElement.CloudSyncHelper.Serialization.DataTypes
         WinVista    = 60,
         Win7        = 61,
         Win8        = 62,
-        Win81      = 63,
+        Win81       = 63,
         Win10       = 100
     }
 
