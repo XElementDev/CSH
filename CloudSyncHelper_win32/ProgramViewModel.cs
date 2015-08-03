b98216941a6b3f4e303069e8076d8b51a5b4439e
CSp <christian.sporer@outlook.com>
2015-08-03 09:57:06 +0200

    * added logic for linking and unlinking

--- CloudSyncHelper_win32/ProgramViewModel.cs
+++ CloudSyncHelper_win32/ProgramViewModel.cs
@@ -1,10 +1,18 @@
-﻿using XElement.CloudSyncHelper.DataTypes;
+﻿using Microsoft.Practices.Prism.Commands;
+using System.Windows.Input;
+using XElement.CloudSyncHelper.DataTypes;
 using XElement.Common.UI;
 
 namespace XElement.CloudSyncHelper.UI.Win32
 {
     public class ProgramViewModel : ViewModelBase
     {
+        public ProgramViewModel()
+        {
+            this.LinkCommand = new DelegateCommand( () => { }, this.LinkCommand_CanExecute );
+            this.UnlinkCommand = new DelegateCommand( () => { }, this.UnlinkCommand_CanExecute );
+        }
+
         public string DisplayName { get { return this._programInfo.DisplayName; } }
 
         private ExecutionLogic _executionLogic;
@@ -27,7 +35,13 @@ namespace XElement.CloudSyncHelper.UI.Win32
             }
         }
 
-        public bool IsInstalled { get { return true; } }
+        public bool IsInstalled { get { return false; } }
+
+        public ICommand LinkCommand { get; private set; }
+        private bool LinkCommand_CanExecute()
+        {
+            return this.IsInstalled && this.HasSuitableConfig;
+        }
 
         private IProgramInfo _programInfo;
         public IProgramInfo ProgramInfo
@@ -39,6 +53,12 @@ namespace XElement.CloudSyncHelper.UI.Win32
                 this.ExecutionLogic = new ExecutionLogic( this.ProgramInfo );
                 this.RaisePropertyChanged( "DisplayName" );
             }
+        }
+
+        public ICommand UnlinkCommand { get; private set; }
+        private bool UnlinkCommand_CanExecute()
+        {
+            return false;
         }
     }
 }
