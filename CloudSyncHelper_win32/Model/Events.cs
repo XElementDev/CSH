8fd9d28174d0ce706d8bd02c0dc63082016cf294
IanStorm <IanStorm28@outlook.com>
2015-08-15 12:07:20 +0200

    * removed currently unused/superfluous stuff

--- CloudSyncHelper_win32/Model/Events.cs
+++ CloudSyncHelper_win32/Model/Events.cs
@@ -1,8 +1,3 @@
-﻿using Microsoft.Practices.Prism.Events;
-
-namespace XElement.CloudSyncHelper.UI.Win32.Events
+﻿namespace XElement.CloudSyncHelper.UI.Win32.Events
 {
-    internal class MefImportsSatisfied : EmptyPresentationEvent { }
-
-    internal class StandardOutputChanged : CompositePresentationEvent<string> { }
 }
