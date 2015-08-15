b86a2d2f2cc9538ceace2d9e9aaf1ed3cf9c43d0
IanStorm <IanStorm28@outlook.com>
2015-08-16 00:06:48 +0200

    * refactored UI element showing supported OSs to a module
     ** ToolTip issues fixed using https://stackoverflow.com/questions/18577521/wpf-data-bindining-a-slider-value-to-a-tooltip-content-string-in-xaml

---
+++ CloudSyncHelper_win32/Modules/SupportedOperatingSystem/View.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace XElement.CloudSyncHelper.UI.Win32.Modules
+{
+    public partial class SupportedOperatingSystemView : UserControl
+    {
+        public SupportedOperatingSystemView()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsOsSupported
+        {
+            get { return (bool)GetValue( IsOsSupportedProperty ); }
+            set { SetValue( IsOsSupportedProperty, value ); }
+        }
+        public static readonly DependencyProperty IsOsSupportedProperty =
+            DependencyProperty.Register( "IsOsSupported", typeof( bool ),
+                                         typeof( SupportedOperatingSystemView ),
+                                         new FrameworkPropertyMetadata( false ) );
+
+        public ImageSource OsImageSource
+        {
+            get { return (ImageSource)GetValue( OsImageSourceProperty ); }
+            set { SetValue( OsImageSourceProperty, value ); }
+        }
+        public static readonly DependencyProperty OsImageSourceProperty =
+            DependencyProperty.Register( "OsImageSource", typeof( ImageSource ),
+                                         typeof( SupportedOperatingSystemView ),
+                                         new FrameworkPropertyMetadata( null ) );
+
+        public string OsName
+        {
+            get { return (string)GetValue( OsNameProperty ); }
+            set { SetValue( OsNameProperty, value ); }
+        }
+        public static readonly DependencyProperty OsNameProperty =
+            DependencyProperty.Register( "OsName", typeof( string ),
+                                         typeof( SupportedOperatingSystemView ),
+                                         new FrameworkPropertyMetadata( String.Empty ) );
+    }
+}
