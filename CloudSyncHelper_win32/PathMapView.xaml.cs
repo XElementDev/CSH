f455042521305a99bed0ad96e5e2920db12e904f
IanStorm <IanStorm28@outlook.com>
2015-08-15 13:43:13 +0200

    * created PathMap UI control

---
+++ CloudSyncHelper_win32/PathMapView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace XElement.CloudSyncHelper.UI.Win32
+{
+    public partial class PathMap : UserControl
+    {
+        public PathMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
