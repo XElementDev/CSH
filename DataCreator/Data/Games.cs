e8d9cc40bfbd444f58e7aa4e5479836d5b890133
IanStorm <IanStorm28@outlook.com>
2015-09-10 21:22:41 +0200

    * added (empty) configuration for "AoE II HD"

--- DataCreator/Data/Games.cs
+++ DataCreator/Data/Games.cs
@@ -6,12 +6,13 @@ using XElement.CloudSyncHelper.Serialization.DataTypes;
 
 namespace XElement.CloudSyncHelper.DataCreator.Data
 {
-    internal static class Games
+    internal static partial class Games
     {
         public static List<AbstractProgramInfo> CreateGameLinkInfos()
         {
             return new List<AbstractProgramInfo>
             {
+                AgeOfEmpiresIIHD(),
                 Anno2070(),
                 Pes2015()
             };
---
+++ DataCreator/Data/Games/AgeOfEmpiresIIHD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using XElement.CloudSyncHelper.Serialization.DataTypes;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data
+{
+    internal static partial class Games
+    {
+        private static GameInfo AgeOfEmpiresIIHD()
+        {
+            return new GameInfo
+            {
+                DisplayName = "Age of Empires II: HD Edition",
+                FolderName = "Age Of Empires 2013 [Age of Empires II_ HD Edition]",
+                OsConfigs = new List<OsConfiguration>
+                {
+                    // Steam Cloud
+                },
+                TechnicalNameMatcher = "Age of Empires II: HD Edition"
+            };
+        }
+    }
+}
