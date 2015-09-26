6a050d90c5ab68f47aa6a9ce53e9cd75c4fa7378
IanStorm <IanStorm28@outlook.com>
2015-09-26 09:41:28 +0200

    * added configuration for Terraria

---
+++ DataCreator/Data/Games/Terraria.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using XElement.CloudSyncHelper.Serialization.DataTypes;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data
+{
+    internal static partial class Games
+    {
+        private static GameInfo Terraria()
+        {
+            return new GameInfo
+            {
+                DisplayName = "Terraria",
+                FolderName = "Terraria 2011 [Terraria]",
+                OsConfigs = new List<OsConfiguration>
+                {
+                    // Steam Cloud
+                },
+                TechnicalNameMatcher = "Terraria"   // TODO: check matcher
+            };
+        }
+    }
+}
