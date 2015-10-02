e1accb44b91ddbc9d8296d58a60db94019d0d99c
IanStorm <IanStorm28@outlook.com>
2015-10-02 13:02:59 +0200

    * renamed RegistryScanner class

--- InstalledPrograms/InstalledPrograms.Implementation/FastRegistryScanner.cs
+++ InstalledPrograms/InstalledPrograms.Implementation/FastRegistryScanner.cs
@@ -6,7 +6,7 @@ namespace XElement.CloudSyncHelper.InstalledPrograms
 #region not unit-tested
     //  --> Based on: http://www.codeproject.com/Tips/782919/Get-List-of-Installed-Applications-of-System-in-Cs
     //      Visited: 2015-08-02
-    public class RegistryScanner : IScanner
+    public class FastRegistryScanner : IScanner
     {
         public IEnumerable<IInstalledProgram> /*IScanner.*/GetInstalledPrograms()
         {
@@ -34,22 +34,6 @@ namespace XElement.CloudSyncHelper.InstalledPrograms
         }
 
         private const string UNINSTALL_KEY = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
-    }
-
-    internal static class Extensions
-    {
-        public static T GetValueOrDefault<T>( this RegistryKey registryKey, string name )
-        {
-            T value = default( T );
-
-            try
-            {
-                value = (T)registryKey.GetValue( name );
-            }
-            catch { }
-
-            return value;
-        }
     }
 #endregion
 }
