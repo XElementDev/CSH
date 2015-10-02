84bcd295c6ecbb0e37368cd8ce4897960956cffb
IanStorm <IanStorm28@outlook.com>
2015-10-02 23:17:21 +0200

    * with the new RegistryScanner all programs are listed correctly

--- InstalledPrograms/InstalledPrograms.MefExtensions/FastRegistryScanner.cs
+++ InstalledPrograms/InstalledPrograms.MefExtensions/FastRegistryScanner.cs
@@ -1,9 +1,7 @@
-﻿using System.ComponentModel.Composition;
-
-namespace XElement.CloudSyncHelper.InstalledPrograms.MefExtensions
+﻿namespace XElement.CloudSyncHelper.InstalledPrograms.MefExtensions
 {
 #region not unit-tested
-    [Export( typeof( IScanner ) )]
+    //[Export( typeof( IScanner ) )]
     internal class FastRegistryScanner : 
         global::XElement.CloudSyncHelper.InstalledPrograms.FastRegistryScanner { }
 #endregion
