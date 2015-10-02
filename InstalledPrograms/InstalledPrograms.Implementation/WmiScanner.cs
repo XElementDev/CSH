d950826e7f6213df3e3f5075eb2c8bad1962289a
IanStorm <IanStorm28@outlook.com>
2015-10-02 13:22:34 +0200

    * added WMI scanner

---
+++ InstalledPrograms/InstalledPrograms.Implementation/WmiScanner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Management;
+
+namespace XElement.CloudSyncHelper.InstalledPrograms
+{
+#region not unit-tested
+    public class WmiScanner : IScanner
+    {
+        //  --> Based on: https://code.msdn.microsoft.com/windowsdesktop/Get-a-list-of-Installed-9620d03d
+        //      Last visited: 2015-09-11
+        public IEnumerable<IInstalledProgram> /*IScanner.*/GetInstalledPrograms()
+        {
+            var result = new List<IInstalledProgram>();
+            var moSearch = new ManagementObjectSearcher( "Select * from Win32_Product" );
+
+            ManagementObjectCollection moReturn = moSearch.Get();
+            foreach ( ManagementObject mo in moReturn )
+            {
+                var nameObj = mo["Name"];
+                if ( nameObj != null )
+                {
+                    var name = nameObj.ToString();
+                    var installedProgram = new InstalledProgram
+                    {
+                        DisplayName = name
+                    };
+                    result.Add( installedProgram );
+                }
+            }
+
+            return result;
+        }
+    }
+#endregion
+}
---
+++ InstalledPrograms/InstalledPrograms.MefExtensions/WmiScanner.cs
@@ -0,0 +1,8 @@
+﻿namespace XElement.CloudSyncHelper.InstalledPrograms.MefExtensions
+{
+#region not unit-tested
+    //[Export( typeof( IScanner ) )]
+    internal class WmiScanner : 
+        global::XElement.CloudSyncHelper.InstalledPrograms.WmiScanner { }
+#endregion
+}
