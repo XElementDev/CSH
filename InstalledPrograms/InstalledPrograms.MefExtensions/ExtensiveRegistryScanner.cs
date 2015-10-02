e36f18a70fffbaa52abb29e5302c0bf1b424013d
IanStorm <IanStorm28@outlook.com>
2015-10-02 13:21:50 +0200

    * added another registry scanner

---
+++ InstalledPrograms/InstalledPrograms.MefExtensions/ExtensiveRegistryScanner.cs
@@ -0,0 +1,8 @@
+﻿namespace XElement.CloudSyncHelper.InstalledPrograms.MefExtensions
+{
+#region not unit-tested
+    //[Export( typeof( IScanner ) )]
+    internal class ExtensiveRegistryScanner : 
+        global::XElement.CloudSyncHelper.InstalledPrograms.ExtensiveRegistryScanner { }
+#endregion
+}
