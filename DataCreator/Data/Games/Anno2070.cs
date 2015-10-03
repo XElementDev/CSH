2de6bae8ae27a24fc70b113630c463853ee87137
IanStorm <IanStorm28@outlook.com>
2015-10-03 15:30:56 +0200

    * added (unchecked) configuration for "ANNO 2070" for Windows 8.1 and Windows 10

--- DataCreator/Data/Games/Anno2070.cs
+++ DataCreator/Data/Games/Anno2070.cs
@@ -17,22 +17,35 @@ namespace XElement.CloudSyncHelper.DataCreator.Data.Games
             this.FolderName = "Anno 2011 [ANNO 2070]";
             this.OsConfigs = new List<OsConfiguration>
             {
-                new OsConfiguration
-                {
-                    Links = new List<AbstractLinkInfo>
-                    {
-                        new FolderLinkInfo
-                        {
-                            DestinationRoot = Environment.SpecialFolder.MyDocuments,
-                            DestinationSubFolderPath = Path.Combine("ANNO 2070", "Accounts", "%UPLAY_ACCOUNT_NAME%"),
-                            DestinationTargetName = "Savegames",
-                            SourceId = "Savegames"
-                        }
-                    },
-                    OsId = OsId.Win81
-                }
+                GetConfigForWin8_1(),   // TODO: Check config for Win8.1
+                GetConfigForWin10()     // TODO: Check config for Win10
             };
             this.TechnicalNameMatcher = "ANNO 2070";    // TODO: check matcher
+        }
+
+        private OsConfiguration GetConfigForWin8_1()
+        {
+            return new OsConfiguration
+            {
+                Links = new List<AbstractLinkInfo>
+                {
+                    new FolderLinkInfo
+                    {
+                        DestinationRoot = Environment.SpecialFolder.MyDocuments,
+                        DestinationSubFolderPath = Path.Combine("ANNO 2070", "Accounts", "%UPLAY_ACCOUNT_NAME%"),
+                        DestinationTargetName = "Savegames",
+                        SourceId = "Savegames"
+                    }
+                },
+                OsId = OsId.Win81
+            };
+        }
+
+        private OsConfiguration GetConfigForWin10()
+        {
+            var win10Config = this.GetConfigForWin8_1();
+            win10Config.OsId = OsId.Win10;
+            return win10Config;
         }
     }
 }
