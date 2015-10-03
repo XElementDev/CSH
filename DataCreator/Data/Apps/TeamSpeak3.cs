706c4aaa5a8c5c5eba3486595a7c2998c5aa01a0
IanStorm <IanStorm28@outlook.com>
2015-10-03 15:30:27 +0200

    * added (unchecked) configuration for "TeamSpeak 3" for Windows 8.1 and Windows 10

--- DataCreator/Data/Apps/TeamSpeak3.cs
+++ DataCreator/Data/Apps/TeamSpeak3.cs
@@ -15,25 +15,36 @@ namespace XElement.CloudSyncHelper.DataCreator.Data.Apps
         {
             this.DisplayName = "TeamSpeak 3";
             this.FolderName = "TeamSpeak 3";
-            // TODO:    --> check configuration
             this.OsConfigs = new List<OsConfiguration>
             {
-                new OsConfiguration
-                {
-                    Links = new List<AbstractLinkInfo>
-                    {
-                        new FileLinkInfo
-                        {
-                            DestinationRoot = Environment.SpecialFolder.ApplicationData,
-                            DestinationSubFolderPath = Path.Combine("TS3Client"),
-                            DestinationTargetName = "settings.db",
-                            SourceId = "settings.db"
-                        }
-                    },
-                    OsId = OsId.Win81
-                }
+                this.GetConfigForWindows8_1(),  // TODO: Check config for Win8.1
+                this.GetConfigForWindows10()    // TODO: Check config for Win10
             };
             this.TechnicalNameMatcher = "TeamSpeak 3 Client";   // TODO: check matcher
+        }
+
+        private OsConfiguration GetConfigForWindows8_1()
+        {
+            return new OsConfiguration { Links = GetLinksForWin81_10(), OsId = OsId.Win81 };
+        }
+
+        private OsConfiguration GetConfigForWindows10()
+        {
+            return new OsConfiguration { Links = GetLinksForWin81_10(), OsId = OsId.Win10 };
+        }
+
+        private List<AbstractLinkInfo> GetLinksForWin81_10()
+        {
+            return new List<AbstractLinkInfo>
+            {
+                new FileLinkInfo
+                {
+                    DestinationRoot = Environment.SpecialFolder.ApplicationData,
+                    DestinationSubFolderPath = Path.Combine("TS3Client"),
+                    DestinationTargetName = "settings.db",
+                    SourceId = "settings.db"
+                }
+            };
         }
     }
 }
