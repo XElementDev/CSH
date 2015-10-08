bee240d5d07c286d0f10a28df7f921f41213b1f9
IanStorm <IanStorm28@outlook.com>
2015-10-08 17:42:27 +0200

    * added configuration for "Plants vs. Zombies"

---
+++ DataCreator/Data/Games/PlantsVsZombies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using XElement.CloudSyncHelper.Serialization.DataTypes;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( GameInfo ) )]
+    public class PlantsVsZombies : GameInfo
+    {
+        [ImportingConstructor]
+        public PlantsVsZombies()
+        {
+            this.DisplayName = "Plants vs. Zombies";
+            this.FolderName = "Plants vs. Zombies 2009 [Pflanzen gegen Zombies]";
+            this.OsConfigs = new List<OsConfiguration>
+            {
+                // Origin
+            };
+            //  2015-09-25: https://stackoverflow.com/questions/3496351/adding-a-tm-superscript-to-a-string
+            this.TechnicalNameMatcher = "Pflanzen gegen Zombies\u2122";  // Plfanzen gegen Zombies™
+        }
+    }
+}
