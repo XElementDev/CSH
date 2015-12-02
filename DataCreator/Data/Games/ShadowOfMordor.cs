c2e873d4eb34fb5ee2d97bf1480a817c6273cbb3
IanStorm <IanStorm28@outlook.com>
2015-12-02 18:39:32 +0100

    * added configuration for "Middle-earth: Shadow of Mordor"

---
+++ DataCreator/Data/Games/ShadowOfMordor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class ShadowOfMordor : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public ShadowOfMordor()
+        {
+            this.DisplayName = "Middle-earth: Shadow of Mordor";
+            this.FolderName = "Middle-earth_ Shadow of Mordor";
+            this.TechnicalNameMatcher = "Middle-earth: Shadow of Mordor";
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.Configuration = this._configFactory.GetSteamCloud();
+        }
+    }
+}
