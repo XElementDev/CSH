a2a7a7ec2cbfff2069fe7b5815f036e4dd76338b
IanStorm <IanStorm28@outlook.com>
2015-12-04 10:26:33 +0100

    * added basic infrastructure for IconCrawler

---
+++ IconCrawler/IconCrawler.Implementation/SteamIconCrawler.cs
@@ -0,0 +1,9 @@
+﻿namespace XElement.CloudSyncHelper.UI.IconCrawler
+{
+#region not unit-tested
+    public class SteamIconCrawler : IIconCrawler
+    {
+        public Reliability Realiability { get { return Reliability.High; } }
+    }
+#endregion
+}
---
+++ IconCrawler/IconCrawler.Interface/IIconCrawler.cs
@@ -0,0 +1,7 @@
+﻿namespace XElement.CloudSyncHelper.UI.IconCrawler
+{
+    public interface IIconCrawler
+    {
+        Reliability Realiability { get; }
+    }
+}
