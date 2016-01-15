569b9dee5731bc6e99e0aefeb1f5845278f296d1
IanStorm <IanStorm28@outlook.com>
2016-01-15 10:16:13 +0100

    * AboutUC is now part of ApplicationMenu
     * know issue: ProductName, Version, etc. properties of AboutViewModel are not set correctly

---
+++ UI/Win32/CloudSyncHelper_win32/Modules/ApplicationMenu/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Composition;
+using AboutViewModel = XElement.CloudSyncHelper.UI.Win32.Modules.About.ViewModel;
+
+namespace XElement.CloudSyncHelper.UI.Win32.Modules.ApplicationMenu
+{
+#region not unit-tested
+    [Export]
+    public class ViewModel
+    {
+        [Import]
+        public AboutViewModel AboutVM { get; private set; }
+    }
+#endregion
+}
