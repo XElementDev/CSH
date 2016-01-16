9f67eba635dc0f068d5a37273f6dae834fb6db1f
IanStorm <IanStorm28@outlook.com>
2016-01-16 23:07:01 +0100

    * ui improvement: fixed 1 pixel too little issue in ApplicationMenu

---
+++ UI/Win32/CloudSyncHelper_win32/Modules/ApplicationMenu/CeilingConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace XElement.CloudSyncHelper.UI.Win32.Modules.ApplicationMenu
+{
+#region not unit-tested
+    internal class CeilingConverter : IValueConverter
+    {
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            double @double = (double)value;
+            var ceiledValue = Math.Ceiling( @double );
+            return ceiledValue;
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            throw new NotImplementedException();
+        }
+    }
+#endregion
+}
