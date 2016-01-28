43d254539ac97ca606a36ecbd24cce53e6d41c9d
IanStorm <IanStorm28@outlook.com>
2016-01-28 13:14:25 +0100

    * added infrastructure for SearchUC

---
+++ UI/Win32/CloudSyncHelper_win32/Modules/Search/View.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace XElement.CloudSyncHelper.UI.Win32.Modules
+{
+    public partial class SearchUC : UserControl
+    {
+        public SearchUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
