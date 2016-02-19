a7f68e9cb05788bb3f02a4c07a3b4f0cbe18fa52
IanStorm <IanStorm28@outlook.com>
2016-02-19 12:23:18 +0100

    * file structure refactoring

--- Serialization/DataTypes.cs
+++ Serialization/DataTypes.cs
@@ -2,26 +2,10 @@
 using System.Xml.Serialization;
 using XElement.CloudSyncHelper.DataTypes;
 using XElement.DotNet.System.Environment;
-using SpecialFolder = System.Environment.SpecialFolder;
 
 namespace XElement.CloudSyncHelper.Serialization.DataTypes
 {
 #region not unit-tested
-    public abstract class AbstractLinkInfo : ILinkInfo
-    {
-        [XmlAttribute( "DestRoot" )]
-        public SpecialFolder DestinationRoot { get; set; }
-
-        [XmlAttribute( "DestSubFolderPath" )]
-        public string DestinationSubFolderPath { get; set; }
-
-        [XmlAttribute( "DestTargetName" )]
-        public string DestinationTargetName { get; set; }
-
-        [XmlAttribute( "SourceId" )]
-        public string SourceId { get; set; }
-    }
-
     public class Definition : IDefinition
     {
         [XmlIgnore]
---
+++ Serialization/DataTypes/AbstractLinkInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+using XElement.CloudSyncHelper.DataTypes;
+using static System.Environment;
+
+namespace XElement.CloudSyncHelper.Serialization.DataTypes
+{
+#region not unit-tested
+    public abstract class AbstractLinkInfo : ILinkInfo
+    {
+        [XmlAttribute( "DestRoot" )]
+        public SpecialFolder DestinationRoot { get; set; }
+
+        [XmlAttribute( "DestSubFolderPath" )]
+        public string DestinationSubFolderPath { get; set; }
+
+        [XmlAttribute( "DestTargetName" )]
+        public string DestinationTargetName { get; set; }
+
+        [XmlAttribute( "SourceId" )]
+        public string SourceId { get; set; }
+    }
+#endregion
+}
