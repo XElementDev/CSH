0959145c67006a65881a1044e3c2dcc4039a3f72
IanStorm <IanStorm28@outlook.com>
2016-02-26 15:15:03 +0100

    * added (empty) configuration for "Far Cry 4"

---
+++ DataCreator/Data/Games/FarCry4.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using XElement.CloudSyncHelper.Serialization.DataTypes;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class FarCry4 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public FarCry4() : base( "CA3C10FC-D045-4431-A73D-B9F06D992CDB" )
+        {
+            this.ApplicationName = "Far Cry 4";
+            this.FolderName = "Far Cry 2014 [Far Cry 4]";
+            this.TechnicalNameMatcher = "Far Cry 4";
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            var osConfigs = new List<OsConfiguration>
+            {
+                // Uplay
+            };
+            this.Definition = this._definitionFactory.Get( osConfigs );
+        }
+    }
+}
