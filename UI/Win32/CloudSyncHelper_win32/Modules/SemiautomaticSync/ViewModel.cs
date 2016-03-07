f697b554d250c786f0249ed2cc6e93dc57e33221
IanStorm <IanStorm28@outlook.com>
2016-03-07 22:12:59 +0100

    * extended boolean condition

--- UI/Win32/CloudSyncHelper_win32/Modules/SemiautomaticSync/ViewModel.cs
+++ UI/Win32/CloudSyncHelper_win32/Modules/SemiautomaticSync/ViewModel.cs
@@ -7,7 +7,9 @@
         {
             this.Model = semiautoSyncModel;
             this.IsAConfigurationAvailable = this.Model.SupportedOSsVM.IsWindows10Supported || 
-                                             this.Model.SupportedOSsVM.IsWindows81Supported;
+                                             this.Model.SupportedOSsVM.IsWindows81Supported || 
+                                             this.Model.SupportedOSsVM.IsWindows8Supported || 
+                                             this.Model.SupportedOSsVM.IsWindows7Supported;
         }
 
         public bool IsAConfigurationAvailable { get; private set; }
