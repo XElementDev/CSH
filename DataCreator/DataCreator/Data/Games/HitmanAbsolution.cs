a0012cac7648aee641892b4cfcee8d3866749932
IanStorm <IanStorm28@outlook.com>
2016-04-08 17:13:57 +0200

    * added (Steam Cloud-only) config for "Hitman: Absolution"

---
+++ DataCreator/DataCreator/Data/Games/HitmanAbsolution.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class HitmanAbsolution : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public HitmanAbsolution() : base( "81A91AE5-E1CD-4F42-B814-CEB590B61B83" )
+        {
+            this.ApplicationName = "Hitman: Absolution";
+            this.FolderName = "Hitman 2012-2014 [Hitman_ Absolution]";
+            this.TechnicalNameMatcher = "Hitman: Absolution";
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.Definition = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
