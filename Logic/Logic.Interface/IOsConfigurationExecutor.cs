bc34172ce809df7e4bc2bbb1d68ef55bed8bfb8d
IanStorm <IanStorm28@outlook.com>
2016-04-18 19:21:43 +0200

    * added logic interfaces

---
+++ Logic/Logic.Interface/IOsConfigurationExecutor.cs
@@ -0,0 +1,15 @@
+﻿using XElement.CloudSyncHelper.DataTypes;
+
+namespace XElement.CloudSyncHelper.Logic
+{
+    public interface IOsConfigurationExecutor
+    {
+        bool IsInCloud( IOsConfiguration osConfig );
+
+        bool IsLinked( IOsConfiguration osConfig );
+
+        void Link( IOsConfiguration osConfig );
+
+        void Unlink( IOsConfiguration osConfig );
+    }
+}
