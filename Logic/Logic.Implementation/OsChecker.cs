957a5ba2b19015d81bdeb6efbce562258ed7fc09
IanStorm <IanStorm28@outlook.com>
2016-04-20 10:25:09 +0200

    * implementation of IOsChecker

---
+++ Logic/Logic.Implementation/OsChecker.cs
@@ -0,0 +1,23 @@
+﻿using XElement.CloudSyncHelper.DataTypes;
+using XElement.DotNet.System.Environment;
+
+namespace XElement.CloudSyncHelper.Logic
+{
+#region not unit-tested
+    public class OsChecker : IOsChecker
+    {
+        public OsChecker( IOsRecognizer osRecognizer )
+        {
+            this._osRecognizer = osRecognizer;
+        }
+
+        public bool IsSuitableForOs( IOsConfigurationInfo osConfig )
+        {
+            var runningOsId = this._osRecognizer.GetOsId();
+            return runningOsId == osConfig.OsId;
+        }
+
+        protected IOsRecognizer _osRecognizer;
+    }
+#endregion
+}
---
+++ Logic/Logic.MefExtensions/OsChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using XElement.DotNet.System.Environment;
+
+namespace XElement.CloudSyncHelper.Logic.MefExtensions
+{
+#region not unit-tested
+    [Export( typeof( IOsChecker ) )]
+    internal class OsChecker : global::XElement.CloudSyncHelper.Logic.OsChecker,
+                               IPartImportsSatisfiedNotification
+    {
+        [ImportingConstructor]
+        private OsChecker() : base( null ) { }
+
+        void IPartImportsSatisfiedNotification.OnImportsSatisfied()
+        {
+            this._osRecognizer = this._osRecognizerMef;
+        }
+
+        [Import]
+        private IOsRecognizer _osRecognizerMef = null;
+    }
+#endregion
+}
