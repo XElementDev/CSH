789cb00b779e6a105249c3a683fc0c54e08b6d6e
IanStorm <IanStorm28@outlook.com>
2016-05-29 18:31:29 +0200

    * added (Steam Cloud-only) configuration for "The Elder Scrolls V: Skyrim"

---
+++ DataCreator/DataCreator/Data/Games/Tes5Skyrim.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Tes5Skyrim : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Tes5Skyrim() : base( "6E5B3B3F-A4E3-4F67-989A-E1E313EF9D24" )
+        {
+            this.ApplicationName = "The Elder Scrolls V: Skyrim";
+            this.FolderName = "TES 2011 [The Elder Scrolls V_ Skyrim]";
+            this.TechnicalNameMatcher = "The Elder Scrolls V: Skyrim";
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
