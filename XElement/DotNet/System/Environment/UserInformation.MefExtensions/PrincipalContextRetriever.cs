990188286bb8af8cdb3c4d77aa4786ff48c6d3ce
IanStorm <IanStorm28@outlook.com>
2016-07-19 11:06:31 +0200

    * removed unused and not working PrincipalContextRetriever

--- XElement/DotNet/System/Environment/UserInformation.MefExtensions/PrincipalContextRetriever.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace XElement.DotNet.System.Environment.UserInformation.MefExtensions
-{
-#region not unit-tested
-    //[Export( typeof( IUserInformationService ) )]
-    internal class PrincipalContextRetriever : 
-        global::XElement.DotNet.System.Environment.UserInformation.PrincipalContextRetriever { }
-#endregion
-}
