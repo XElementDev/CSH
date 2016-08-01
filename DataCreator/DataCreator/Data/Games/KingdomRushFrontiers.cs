4fcb601a6385bc3e684efd72d151b2f20437744e
IanStorm <IanStorm28@outlook.com>
2016-08-01 17:57:31 +0200

    * added (Steam Cloud-only) configuration for "Kingdom Rush Frontiers"

---
+++ DataCreator/DataCreator/Data/Games/KingdomRushFrontiers.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class KingdomRushFrontiers : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public KingdomRushFrontiers() : base( "998B35CD-1C88-41AE-8205-3D7D634E5E82" )
+        {
+            this.ApplicationName = "Kingdom Rush Frontiers";
+            this.FolderName = "Kingdom Rush 2016 [Kingdom Rush Frontiers]";
+            this.TechnicalNameMatcher = "Kingdom Rush Frontiers";
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
