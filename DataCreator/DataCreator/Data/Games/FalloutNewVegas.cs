d818446730d74d9c62f9d91c57cbc0be67fd3ccb
IanStorm <IanStorm28@outlook.com>
2016-08-03 12:04:20 +0200

    * added (Steam Cloud-only) configuration for "Fallout: New Vegas"

---
+++ DataCreator/DataCreator/Data/Games/FalloutNewVegas.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class FalloutNewVegas : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public FalloutNewVegas() : base( "82423A58-05E4-4A40-9086-A49ADD4D7620" )
+        {
+            this.ApplicationName = "Fallout: New Vegas";
+            this.FolderName = "Fallout 2010-2011 [Fallout_ New Vegas]";
+            this.TechnicalNameMatcher = "Fallout: New Vegas";
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
