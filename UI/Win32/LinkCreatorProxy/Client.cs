8d7ba89ba1854772001146afc0dda0191bee83de
IanStorm <IanStorm28@outlook.com>
2016-08-03 19:32:09 +0200

    * basic client server communication possible in LinkCreatorProxy

---
+++ UI/Win32/LinkCreatorProxy/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace XElement.CloudSyncHelper.UI.Win32
+{
+#region not unit-tested
+    internal class Client
+    {
+        public Client( /*string message*/ )
+        {
+            this.StartClient();
+        }
+
+
+        public void Loop()
+        {
+            StreamWriter writer = new StreamWriter( this._clientPipe );
+
+            while ( true )
+            {
+                string input = Console.ReadLine();
+                if ( String.IsNullOrEmpty( input ) ) break;
+                writer.WriteLine( input );
+                writer.Flush();
+            }
+        }
+
+
+        private void StartClient()
+        {
+            var client = new NamedPipeClientStream( Program.PIPE_NAME );
+            client.Connect();
+            this._clientPipe = client;
+            Console.WriteLine( "Client connected." );
+        }
+
+
+        private Stream _clientPipe;
+    }
+#endregion
+}
---
+++ UI/Win32/LinkCreatorProxy/Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+
+namespace XElement.CloudSyncHelper.UI.Win32
+{
+#region not unit-tested
+    //  --> https://stackoverflow.com/questions/13806153/example-of-named-pipes
+    //      Last visited: 2016-08-03
+    internal class Server
+    {
+        public Server( /*string message*/ )
+        {
+            this.AnotherInstanceAlreadyRunning = false;
+            this.TryStartServer();
+        }
+
+
+        public bool AnotherInstanceAlreadyRunning { get; private set; }
+
+
+        public void Loop()
+        {
+            this._server.WaitForConnection();
+            StreamReader reader = new StreamReader( this._server );
+            while ( true )
+            {
+                var line = reader.ReadLine();
+                if ( line != null )
+                {
+                    Console.WriteLine( String.Join( "", line.Reverse() ) );
+                }
+            }
+        }
+
+
+        private void StartServer()
+        {
+            this._server = new NamedPipeServerStream( Program.PIPE_NAME );
+            Console.WriteLine( "Server up and running." );
+        }
+
+
+        private void TryStartServer()
+        {
+            try
+            {
+                this.StartServer();
+            }
+            catch ( IOException )
+            {
+                this.AnotherInstanceAlreadyRunning = true;
+            }
+        }
+
+
+        private NamedPipeServerStream _server;
+    }
+#endregion
+}
