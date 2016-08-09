ae0fdfa8b7e1e5f93294036f8eb10ce43b44dd5f
IanStorm <IanStorm28@outlook.com>
2016-08-09 15:27:13 +0200

    * implemented adapter for LinkCreator service
    * expected behavior: build pass, tests run, win32 application runs

---
+++ UI/Win32/LinkCreatorServiceAdapter/ExecutorProxy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using XElement.CloudSyncHelper.Logic.Execution.MkLink;
+using XElement.CloudSyncHelper.UI.Win32.LinkCreator.Serialization;
+
+namespace XElement.CloudSyncHelper.UI.Win32.LinkCreatorServiceAdapter
+{
+#region not unit-tested
+    internal class ExecutorProxy : IExecutor
+    {
+        public ExecutorProxy( ParametersDTO parameters )
+        {
+            this._parameters = parameters;
+            this._serializationManager = new Manager();
+        }
+
+
+        private string Arguments
+        {
+            get { return this._serializationManager.Serialize( this._parameters ); }
+        }
+
+
+        public void Execute()
+        {
+            var process = new Process();
+            process.StartInfo.FileName = this.PathToLinkCreatorSvcExe;
+            process.StartInfo.Arguments = this.Arguments;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+        }
+
+
+        private string PathToLinkCreatorSvcExe
+        {
+            get
+            {
+                return Path.Combine( Environment.CurrentDirectory, 
+                                     SERVICE_FILE_NAME );
+            }
+        }
+
+
+        private const string SERVICE_FILE_NAME = "CSH_LinkCreatorProxy_win32.exe";
+
+
+        private ParametersDTO _parameters;
+        private IManager _serializationManager;
+    }
+#endregion
+}
---
+++ UI/Win32/LinkCreatorServiceAdapter/MkLinkExecutorFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+using XElement.CloudSyncHelper.Logic.Execution.MkLink;
+
+namespace XElement.CloudSyncHelper.UI.Win32.LinkCreatorServiceAdapter
+{
+#region not unit-tested
+    [Export( typeof( Logic.Execution.MkLink.IFactory ) )]
+    internal class MkLinkExecutorFactory : Logic.Execution.MkLink.IFactory
+    {
+        private MkLinkExecutorFactory() { }
+
+
+        public IExecutor Get( ParametersDTO parameters )
+        {
+            return new ExecutorProxy( parameters );
+        }
+    }
+#endregion
+}
