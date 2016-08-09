e46c8a8444ca8814f60d01708ec27af0ddd292ad
IanStorm <IanStorm28@outlook.com>
2016-08-09 10:03:35 +0200

    * initial commit of project that take cares of LinkCreator's serialization

---
+++ UI/Win32/LinkCreator/Serialization/IManager.cs
@@ -0,0 +1,12 @@
+﻿using XElement.CloudSyncHelper.Logic.Execution.MkLink;
+
+namespace XElement.CloudSyncHelper.UI.Win32.LinkCreator.Serialization
+{
+    public interface IManager
+    {
+        ParametersDTO Deserialize( string serialized );
+
+
+        string Serialize( ParametersDTO parameters );
+    }
+}
