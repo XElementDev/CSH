874a3cb0f7170c08af7a85e558e3ac2443e4a94c
IanStorm <IanStorm28@outlook.com>
2016-10-09 21:30:33 +0200

    * added (Steam Cloud-only) configuration for "Braid"

---
+++ DataCreator/DataCreator/Data/Games/Braid.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Braid : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Braid() : base( "FDC6616E-D674-4148-A716-FE0DA3584F67" )
+        {
+            this.ApplicationName = "Braid";
+            this.FolderName = "Braid 2009 [Braid]";
+            this.TechnicalNameMatcher = "Braid";
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
