09fa45184137d09a65a25d95b71240b74bce1cb1
IanStorm <IanStorm28@outlook.com>
2016-10-11 20:18:11 +0200

    * added (Steam Cloud-only) configuration for "LIMBO"

---
+++ DataCreator/DataCreator/Data/Games/LIMBO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Limbo : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Limbo() : base( "803F2C35-45C2-4A36-A3C4-9406EF99B89D" )
+        {
+            this.ApplicationName = "LIMBO";
+            this.FolderName = "Limbo 2011 [LIMBO]";
+            this.TechnicalNameMatcher = "LIMBO";
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
