fb7aea495acaeb32ff13bac0fe2ab5c9c2b316de
IanStorm <IanStorm28@outlook.com>
2017-01-20 12:43:48 +0100

    * added (Steam Cloud-only) configuration for "Draw a Stickman: EPIC"

---
+++ DataCreator/DataCreator/Data/Games/DrawAStickmanEpic.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class DrawAStickmanEpic : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public DrawAStickmanEpic() : base( "2BA555A1-99AE-4543-9713-C97F83149C8A" )
+        {
+            this.ApplicationName = "Draw a Stickman: EPIC";
+            this.FolderName = "Draw a Stickman 2013 [Draw a Stickman_ EPIC]";
+            this.TechnicalNameMatcher = "Draw a Stickman: EPIC";
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
