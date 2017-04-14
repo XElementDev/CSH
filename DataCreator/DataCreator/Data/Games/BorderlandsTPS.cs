929b66645963b8c9dbccb04fcaef15598185a3ae
IanStorm <IanStorm28@outlook.com>
2017-04-14 12:36:13 +0200

    * added (Steam Cloud-only) configuration for "Borderlands: The Pre-Sequel"

---
+++ DataCreator/DataCreator/Data/Games/BorderlandsTPS.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class BorderlandsTPS : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public BorderlandsTPS() : base( "ABEBD728-DEC6-4CCB-8130-AC281B61A49C" )
+        {
+            this.ApplicationName = "Borderlands: The Pre-Sequel";
+            this.FolderName = "Borderlands 2014 [Borderlands_ The Pre-Sequel]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
