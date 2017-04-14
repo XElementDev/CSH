7517e52e504966ebf0de3ab7bea44d923dcb417e
IanStorm <IanStorm28@outlook.com>
2017-04-14 12:44:41 +0200

    * added (Steam Cloud-only) configuration for "Counter-Strike: Global Offensive"

---
+++ DataCreator/DataCreator/Data/Games/CounterStrikeGO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class CounterStrikeGO : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public CounterStrikeGO() : base( "CD027C0C-9ED5-4B81-B0AB-4EA5C0269D66" )
+        {
+            this.ApplicationName = "Counter-Strike: Global Offensive";
+            this.FolderName = "Counter-Strike 2012 [Counter-Strike_ Global Offensive]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
