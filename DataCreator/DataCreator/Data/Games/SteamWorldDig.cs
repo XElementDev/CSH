821cdd456e8f6fc6c164ea2f6759a60b5067d902
IanStorm <IanStorm28@outlook.com>
2017-04-14 12:54:12 +0200

    * added (Steam Cloud-only) configuration for "SteamWorld Dig"

---
+++ DataCreator/DataCreator/Data/Games/SteamWorldDig.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class SteamWorldDig : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public SteamWorldDig() : base( "464D0492-C5A6-49AE-B425-12ED3A46B082" )
+        {
+            this.ApplicationName = "SteamWorld Dig";
+            this.FolderName = "SteamWorld 2013 [SteamWorld Dig]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
