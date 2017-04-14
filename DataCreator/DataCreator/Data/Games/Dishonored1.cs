21d0e7ad02be5d124c4e229a256bcb76bc8de80e
IanStorm <IanStorm28@outlook.com>
2017-04-14 12:48:54 +0200

    * added (Steam Cloud-only) configuration for "Dishonored"

---
+++ DataCreator/DataCreator/Data/Games/Dishonored1.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Dishonored1 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Dishonored1() : base( "934B0E8F-0962-4609-A6C4-7DDAE9E0848E" )
+        {
+            this.ApplicationName = "Dishonored";
+            this.FolderName = "Dishonored 2012 [Dishonored]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
