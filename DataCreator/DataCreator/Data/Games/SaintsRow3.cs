85ff2d964e4a7ddf275bf8b169c76f09813d8453
IanStorm <IanStorm28@outlook.com>
2017-04-14 12:26:31 +0200

    * added (Steam Cloud-only) configuration for "Saints Row: The Third"

---
+++ DataCreator/DataCreator/Data/Games/SaintsRow3.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class SaintsRow3 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public SaintsRow3() : base( "5D92770F-DA68-4ADA-BCDD-B0D42B8B5833" )
+        {
+            this.ApplicationName = "Saints Row: The Third";
+            this.FolderName = "Saints Row 2011 [Saints Row_ The Third]";
+            this.TechnicalNameMatcher = "Saints Row: The Third";
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
