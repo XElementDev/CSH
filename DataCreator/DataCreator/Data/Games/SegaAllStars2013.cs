1f4c298ee0ba4c1d180250b1a1b250bf5ef89deb
IanStorm <IanStorm28@outlook.com>
2017-04-14 12:59:21 +0200

    * added (Steam Cloud-only) configuration for "Sonic & All-Stars Racing Transformed"

---
+++ DataCreator/DataCreator/Data/Games/SegaAllStars2013.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class SegaAllStars2013 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public SegaAllStars2013() : base( "0074BFE1-1954-4A3B-9ACE-A313F9C868FE" )
+        {
+            this.ApplicationName = "Sonic & All-Stars Racing Transformed";
+            this.FolderName = "Sega All-Stars 2013 [Sonic _ All-Stars Racing Transformed]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
