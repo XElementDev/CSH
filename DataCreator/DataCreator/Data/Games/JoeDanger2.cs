817ba6efaaa8e258b609f76ee02fc285b5c5a001
IanStorm <IanStorm28@outlook.com>
2017-05-16 13:00:30 +0200

    * added (Steam Cloud-only) configuration for "Joe Danger 2: The Movie"

---
+++ DataCreator/DataCreator/Data/Games/JoeDanger2.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class JoeDanger2 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public JoeDanger2() : base( "CD27DCA3-D440-4B2E-AA72-78F47AB9D07E" )
+        {
+            this.ApplicationName = "Joe Danger 2: The Movie";
+            this.FolderName = "Joe Danger 2013 [Joe Danger 2_ The Movie]";
+            this.TechnicalNameMatcher = this.ApplicationName; // TODO: check matcher
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
