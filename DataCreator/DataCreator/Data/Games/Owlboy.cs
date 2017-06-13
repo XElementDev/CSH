7aae49dbc7dfcc09453c8ed1baed91bb5e47cabd
IanStorm <IanStorm28@outlook.com>
2017-06-13 17:59:40 +0200

    * added (Steam Cloud-only) configuration for "Owlboy"

---
+++ DataCreator/DataCreator/Data/Games/Owlboy.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Owlboy : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Owlboy() : base( "3B69007D-6EB1-423D-9A56-8FE7C5BAC404" )
+        {
+            this.ApplicationName = "Owlboy";
+            this.FolderName = "Owlboy 2016 [Owlboy]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
