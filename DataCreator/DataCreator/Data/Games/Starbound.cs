c3831ccdbae1843d03261047aafa7f7e14e8d985
IanStorm <IanStorm28@outlook.com>
2017-07-27 15:12:35 +0200

    * added (Steam Cloud-only) configuration for "Starbound"

---
+++ DataCreator/DataCreator/Data/Games/Starbound.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Starbound : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Starbound() : base( "E33D0478-9ABC-49DC-9FA0-C9312BC5CF91" )
+        {
+            this.ApplicationName = "Starbound";
+            this.FolderName = "Starbound 2013 [Starbound]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
