a056e5715095e94496f520fbc16a67397d284ff8
IanStorm <IanStorm28@outlook.com>
2017-07-27 15:11:55 +0200

    * added (Steam Cloud-only) configuration for "Risk of Rain"

---
+++ DataCreator/DataCreator/Data/Games/RiskOfRain.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class RiskOfRain : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public RiskOfRain() : base( "F0C7E626-408D-4C70-9B41-83C6C7C6BBEC" )
+        {
+            this.ApplicationName = "Risk of Rain";
+            this.FolderName = "Risk of Rain 2013 [Risk of Rain]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
