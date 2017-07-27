9a2e3f796ba0278eacc0be1041a10ebb4404fef1
IanStorm <IanStorm28@outlook.com>
2017-07-27 15:09:18 +0200

    * added (Steam Cloud-only) configuration for "Forge Quest"

---
+++ DataCreator/DataCreator/Data/Games/ForgeQuest.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class ForgeQuest : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public ForgeQuest() : base( "E1D567DE-EB0D-429D-B9AE-634A6D5952F7" )
+        {
+            this.ApplicationName = "Forge Quest";
+            this.FolderName = "Forge Quest 2015 [Forge Quest]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
