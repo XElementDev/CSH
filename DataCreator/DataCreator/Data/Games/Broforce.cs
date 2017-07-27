d7f23021894b4ed76befe34c69a6ca88ca741747
IanStorm <IanStorm28@outlook.com>
2017-07-27 15:08:20 +0200

    * added (Steam Cloud-only) configuration for "Broforce"

---
+++ DataCreator/DataCreator/Data/Games/Broforce.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Broforce : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Broforce() : base( "0E472D3F-D542-437A-8E29-B86563F664B6" )
+        {
+            this.ApplicationName = "Broforce";
+            this.FolderName = "Broforce 2014 [Broforce]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
