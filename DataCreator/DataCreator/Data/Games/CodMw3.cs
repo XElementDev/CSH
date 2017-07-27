860f8d7863262ec53093eb13f38647336feae663
IanStorm <IanStorm28@outlook.com>
2017-07-27 15:07:27 +0200

    * added (Steam Cloud-only) configuration for "Call of Duty: Modern Warfare 3"

---
+++ DataCreator/DataCreator/Data/Games/CodMw3.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class CodMw3 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public CodMw3() : base( "5015F2D9-3547-4958-8CBE-CDF8E91B43A1" )
+        {
+            this.ApplicationName = "Call of Duty: Modern Warfare 3";
+            this.FolderName = "Call of Duty 2011 [Call of Duty_ Modern Warfare 3]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+        }
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+        }
+    }
+}
