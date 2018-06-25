c5956244f9312caa158acfc0a1158daf1f7da5b2
IanStorm <IanStorm28@outlook.com>
2018-06-25 21:43:09 +0200

    added very basic unit test(s) for testing the user information retrieval logic

---
+++ XElement/DotNet/System/Environment/Test_UserInformation.MefExtensions/testMergeAllRetriever.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Reflection;
+
+namespace XElement.DotNet.System.Environment.UserInformation.MefExtensions
+{
+    [TestClass]
+    public class testMergeAllRetriever
+    {
+        [TestMethod]
+        public void testMergeAllRetriever_IsExportedViaMef()
+        {
+            var mefHelper = new testMergeAllRetriever.MefHelper();
+            var container = this.CreateMefContainer();
+            container.ComposeParts( mefHelper );
+
+            Assert.IsNotNull( mefHelper.UserInformation );
+        }
+
+
+
+        private CompositionContainer CreateMefContainer()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var path = Path.GetDirectoryName( assembly.Location );
+            var catalog = new DirectoryCatalog( path );
+            var container = new CompositionContainer( catalog );
+            return container;
+        }
+
+
+
+        [Export]
+        private class MefHelper
+        {
+            [Import]
+            public IUserInformation UserInformation { get; private set; }
+        }
+    }
+}
