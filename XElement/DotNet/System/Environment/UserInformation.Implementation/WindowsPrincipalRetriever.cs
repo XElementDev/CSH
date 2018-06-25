5912fab4355be3c72ab56ab1cf4959f81e11d1ec
IanStorm <IanStorm28@outlook.com>
2018-06-25 21:42:16 +0200

    `WindowsPrincipalRetriever` can retrieve more information now

--- XElement/DotNet/System/Environment/UserInformation.Implementation/WindowsPrincipalRetriever.cs
+++ XElement/DotNet/System/Environment/UserInformation.Implementation/WindowsPrincipalRetriever.cs
@@ -1,34 +1,68 @@
-﻿using System.Security.Principal;
+﻿using System;
+using System.Security.Principal;
 using RoleEnum = XElement.DotNet.System.Environment.UserInformation.Role;
 
 namespace XElement.DotNet.System.Environment.UserInformation
 {
 #region not unit-tested
-    public class WindowsPrincipalRetriever : IRoleInformation
+    public class WindowsPrincipalRetriever : IUserInformationInt
     {
         public WindowsPrincipalRetriever()
         {
-            this.Role = WindowsPrincipalRetriever.GetRole();
+            var identity = WindowsPrincipalRetriever.GetIdentity();
+
+            var windowsLogonName = WindowsPrincipalRetriever.GetWindowsLogonName( identity );
+            var splittedLogonInfo = windowsLogonName.Split( new[] { "\\" }, 
+                                                            StringSplitOptions.RemoveEmptyEntries );
+
+            this.Domain = splittedLogonInfo[0];
+            this.FullName = null;
+            this.Role = WindowsPrincipalRetriever.GetRole( identity );
+            this.TechnicalName = splittedLogonInfo[1];
+
+            return;
         }
 
 
-        public RoleEnum? /*IRoleInformation.*/Role { get; private set; }
+        public string /*IUserInformationInt.*/Domain { get; private set; }
+
+
+        public string /*IUserInformationInt.*/FullName { get; private set; }
+
+
+        private static WindowsIdentity GetIdentity()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            return identity;
+        }
 
 
         //  --> https://stackoverflow.com/questions/3600322/check-if-the-current-user-is-administrator
         //      Last visited: 2016-07-17
-        private static RoleEnum GetRole()
+        private static RoleEnum GetRole( WindowsIdentity identity )
         {
-            RoleEnum result = RoleEnum.User;
+            RoleEnum role = RoleEnum.User;
 
-            WindowsIdentity identity = WindowsIdentity.GetCurrent();
             WindowsPrincipal principal = new WindowsPrincipal( identity );
             var isAdmin = principal.IsInRole( WindowsBuiltInRole.Administrator );
             if ( isAdmin )
-                result = RoleEnum.Administrator;
+                role = RoleEnum.Administrator;
 
-            return result;
+            return role;
         }
+
+
+        private static string GetWindowsLogonName( WindowsIdentity identity )
+        {
+            var windowsLogonName = identity.Name;
+            return windowsLogonName;
+        }
+
+
+        public RoleEnum? /*IUserInformationInt.*/Role { get; private set; }
+
+
+        public string /*IUserInformationInt.*/TechnicalName { get; private set; }
     }
 #endregion
 }
--- XElement/DotNet/System/Environment/UserInformation.MefExtensions/WindowsPrincipalRetriever.cs
+++ XElement/DotNet/System/Environment/UserInformation.MefExtensions/WindowsPrincipalRetriever.cs
@@ -3,7 +3,7 @@
 namespace XElement.DotNet.System.Environment.UserInformation.MefExtensions
 {
 #region not unit-tested
-    [Export( typeof( IRoleInformation ) )]
+    [Export( typeof( IUserInformationInt ) )]
     internal class WindowsPrincipalRetriever : 
         global::XElement.DotNet.System.Environment.UserInformation.WindowsPrincipalRetriever { }
 #endregion
