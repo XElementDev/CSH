4a61d6c2cd9957d3c1c483b7df47ade5f602ab54
IanStorm <IanStorm28@outlook.com>
2018-06-30 14:10:44 +0200

    added (Steam Cloud-only) configuration for "Goat Simulator"

---
+++ DataCreator/DataCreator/Data/Games/GoatSimulator.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class GoatSimulator : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public GoatSimulator() : base( "C7307409-5748-456C-928A-29B6B1C4B630" )
+        {
+            this.ApplicationName = "Goat Simulator";
+            this.FolderName = "Goat Simulator 2014 [Goat Simulator]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
