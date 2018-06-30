2feb48ab83900165cf17d0a8071702e89df8e8b8
IanStorm <IanStorm28@outlook.com>
2018-06-30 14:10:24 +0200

    added (Steam Cloud-only) configuration for "Football Manager 2017" and "Football Manager Touch 2017"

---
+++ DataCreator/DataCreator/Data/Games/FM2017.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class FootballManager2017 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public FootballManager2017() : base( "8F2F4639-74B5-4CC8-AE9B-9177ED20AE45" )
+        {
+            this.ApplicationName = "Football Manager 2017";
+            this.FolderName = "FM 2016 [Football Manager 2017]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
---
+++ DataCreator/DataCreator/Data/Games/FM2017Touch.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class FootballManager2017Touch : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public FootballManager2017Touch() : base( "9FD826FC-7AEB-4CD3-AEF6-B670D6DFC826" )
+        {
+            this.ApplicationName = "Football Manager Touch 2017";
+            this.FolderName = "FM 2016 [Football Manager Touch 2017]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
