e8f19b015051f999b02cfd410b737b571b5721b4
IanStorm <IanStorm28@outlook.com>
2018-06-30 19:46:05 +0200

    added (Win 10) configuration for "Reus"

---
+++ DataCreator/DataCreator/Data/Games/Reus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using XElement.CloudSyncHelper.Serialization.DataTypes;
+using XElement.DotNet.System.Environment;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Reus : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Reus() : base( "DE8BF438-D9D5-442B-A1E9-93685B4ED382" )
+        {
+            this.ApplicationName = "Reus";
+            this.FolderName = "Reus 2013 [Reus]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        private List<AbstractLinkInfo> GetLinksForWin10()
+        {
+            var destTargetName = "Saves";
+            return new List<AbstractLinkInfo>
+            {
+                new FolderLinkInfo
+                {
+                    DestinationRoot = Environment.SpecialFolder.MyDocuments,
+                    DestinationSubFolderPath = Path.Combine("Reus"),
+                    DestinationTargetName = destTargetName,
+                    SourceId = destTargetName
+                }
+            };
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            var osConfigs = new List<OsConfigurationInfo>
+            {
+                this._osConfigFactory.Get( this.GetLinksForWin10(), OsId.Win10 )
+            };
+            this.DefinitionInfo = this._definitionFactory.Get( osConfigs );
+            return;
+        }
+    }
+}
