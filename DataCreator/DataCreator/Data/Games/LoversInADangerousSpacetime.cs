b6b61a8b616de4b953c7b62b1b0ae79943f28bbf
IanStorm <IanStorm28@outlook.com>
2018-06-30 19:41:07 +0200

    added (Steam Cloud-only) configuration for "Lovers in a Dangerous Spacetime"

---
+++ DataCreator/DataCreator/Data/Games/LoversInADangerousSpacetime.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class LoversInADangerousSpacetime : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public LoversInADangerousSpacetime() : base( "3526A2B7-715C-4D22-88AD-BCDB5E129D9B" )
+        {
+            this.ApplicationName = "Lovers in a Dangerous Spacetime";
+            this.FolderName = this.ApplicationName;
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
