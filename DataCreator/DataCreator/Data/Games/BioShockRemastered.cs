51479badeb73eb18e8b50bc6edddb745fdffef58
IanStorm <IanStorm28@outlook.com>
2018-06-30 14:08:19 +0200

    added (Steam Cloud-only) configuration for "BioShock Remastered"

---
+++ DataCreator/DataCreator/Data/Games/BioShockRemastered.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class BioshockRemastered : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public BioshockRemastered() : base( "9B336EA3-FF18-4158-8264-1481DF91A4BB" )
+        {
+            this.ApplicationName = "BioShock Remastered";
+            this.FolderName = "BioShock 2016 [BioShock Remastered]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
