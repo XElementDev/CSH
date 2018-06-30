64b1c80df7c8827f4119f77bf319d2bec88f9c23
IanStorm <IanStorm28@outlook.com>
2018-06-30 14:08:48 +0200

    added (Steam Cloud-only) configuration for "Dungeon of the Endless"

---
+++ DataCreator/DataCreator/Data/Games/DungeonOfTheEndless.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class DungeonOfTheEndless : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public DungeonOfTheEndless() : base( "50A14F8B-52CC-4CBD-8108-C94586DE621F" )
+        {
+            this.ApplicationName = "Dungeon of the Endless";
+            this.FolderName = "Endless 2014 [Dungeon of the Endless]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
