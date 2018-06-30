bb45166a514fe0a83d510fd7f357f747046d10f6
IanStorm <IanStorm28@outlook.com>
2018-06-30 14:07:48 +0200

    added (Steam Cloud-only) configuration for "Batman: Arkham Origins"

---
+++ DataCreator/DataCreator/Data/Games/BatmanArkhamOrigins.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+using XElement.DotNet.System.Text;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class BatmanArkhamOrigins : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public BatmanArkhamOrigins() : base( "34AA8A3F-AE4E-4876-B5C5-F93712623CC1" )
+        {
+            this.ApplicationName = $"Batman{SpecialCharacters.TRADEMARK}: Arkham Origins";
+            this.FolderName = "Batman Arkham 2013 [Batman_ Arkham Origins]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
