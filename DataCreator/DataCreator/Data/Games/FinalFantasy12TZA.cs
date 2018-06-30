d422d44fb3543f1e5b5dd3951ff4031d75f51b0d
IanStorm <IanStorm28@outlook.com>
2018-06-30 14:09:38 +0200

    added (Steam Cloud-only) configuration for "Final Fantasy XII The Zodiac Age"

---
+++ DataCreator/DataCreator/Data/Games/FinalFantasy12TZA.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class FinalFantasy12Tza : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public FinalFantasy12Tza() : base( "0FA4567F-5328-4D9A-852D-BB8FE74B508A" )
+        {
+            this.ApplicationName = "Final Fantasy XII The Zodiac Age";
+            this.FolderName = "Final Fantasy 2018 [Final Fantasy XII TZA]";
+            this.TechnicalNameMatcher = this.ApplicationName.ToUpper();
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
