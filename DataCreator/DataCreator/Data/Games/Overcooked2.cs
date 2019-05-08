6d6a068b08b35fa5f9c68df5dcd201fe6a873d93
Christian Sporer <IanStorm28@outlook.com>
2019-05-08 20:07:45 +0200

    Added (Steam Cloud-only) configuration for "Overcooked! 2"

---
+++ DataCreator/DataCreator/Data/Games/Overcooked2.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Overcooked2 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Overcooked2() : base( "2108BD77-A9C3-4098-875E-3436EF346768" )
+        {
+            this.ApplicationName = "Overcooked! 2";
+            this.FolderName = "Overcooked 2018 [Overcooked 2]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
