514b50a4ca3914d236a8866cce3db1c605633af0
Christian Sporer <IanStorm28@outlook.com>
2019-07-20 11:35:43 +0200

    Added (Steam Cloud-only) configuration for "Monaco"

---
+++ DataCreator/DataCreator/Data/Games/Monaco.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Monaco : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Monaco() : base( "FC01571B-68A1-488A-9437-183F09FCBAC8" )
+        {
+            this.ApplicationName = "Monaco";
+            this.FolderName = $"Monaco 2013 [{this.ApplicationName}]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            // SteamID: 113020
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
