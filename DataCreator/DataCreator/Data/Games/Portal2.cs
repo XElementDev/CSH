e0c9e6ddd97c69b2a1310e35ebc1dff018d5afa9
Christian Sporer <IanStorm28@outlook.com>
2019-07-20 11:34:44 +0200

    Added (Steam Cloud-only) configuration for "Portal 2"

---
+++ DataCreator/DataCreator/Data/Games/Portal2.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Portal2 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Portal2() : base( "270A4DED-FC78-458A-9391-770D3BEE483D" )
+        {
+            this.ApplicationName = "Portal 2";
+            this.FolderName = $"Portal 2011 [{this.ApplicationName}]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            // SteamID: 620
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
