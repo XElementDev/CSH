b1f73dc4c8403270846f62c637c853217e41ea95
Christian Sporer <IanStorm28@outlook.com>
2019-07-20 11:34:13 +0200

    Added (Steam Cloud-only) configuration for "Project CARS 2"

---
+++ DataCreator/DataCreator/Data/Games/ProjectCars2.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class ProjectCars2 : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public ProjectCars2() : base( "DA07EBC9-1814-410C-9A24-21C23889B8F9" )
+        {
+            this.ApplicationName = "Project CARS 2";
+            this.FolderName = $"Project Cars 2017 [{this.ApplicationName}]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            // SteamID: 378860
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
