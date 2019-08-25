818a99ca94f69808ba4294712f55ef736a7a21cc
Christian Sporer <IanStorm28@outlook.com>
2019-08-25 18:19:21 +0200

    Added (Steam Cloud-only) configuration for "Firewatch"

---
+++ DataCreator/DataCreator/Data/Games/Firewatch.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+
+namespace XElement.CloudSyncHelper.DataCreator.Data.Games
+{
+    [Export( typeof( AbstractGameInfo ) )]
+    internal class Firewatch : AbstractGameInfo
+    {
+        [ImportingConstructor]
+        public Firewatch() : base( "45732ED8-B566-4285-875A-602DA56A6FC0" )
+        {
+            this.ApplicationName = "Firewatch";
+            this.FolderName = $"Firewatch 2016 [{this.ApplicationName}]";
+            this.TechnicalNameMatcher = this.ApplicationName;
+            // SteamID: 383870
+            return;
+        }
+
+
+        protected override void OnImportsSatisfied()
+        {
+            this.DefinitionInfo = this._definitionFactory.GetSteamCloud();
+            return;
+        }
+    }
+}
